89fca71bb8e49572cc3c6fc6c776e5376255d630
Dylan Butler <dylanbutler@outlook.com.au>
2023-02-15 14:31:55 +1100

    Fix deploy actions (#50)
    * simplify
    
    * update actions
    
    * update actions
    
    * update actions
    
    * update actions
    
    * update actions
    
    * update actions
    
    * update actions
    
    * update image build
    
    * update image build
    
    * update action filename
    
    * update deploy action
    
    * update deploy action
    
    * update deploy action
    
    * update deploy action
    
    * fix dockerfile
    
    * remove import
    
    * remove log

--- shortener/pkg/configs/load.go
+++ shortener/pkg/configs/load.go
@@ -3,7 +3,6 @@ package configs
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -37,8 +36,6 @@ func findBytes() ([]byte, error) {
 	if configFile := os.Getenv("CONFIG_FILE"); configFile != "" {
 		file = configFile
 	}
-
-	log.Printf("config: %s", file)
 
 	if _, err := os.Stat(file); err != nil {
 		return nil, errors.New("config file not found")
