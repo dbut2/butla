a2f543b0ee3c1a734e260822dad33c95f85cfef0
Dylan Butler <dylan@but.la>
2024-07-06 18:52:58 +1000

    improve amihome

--- amihome/main.go
+++ amihome/main.go
@@ -5,7 +5,6 @@ import (
 	"html/template"
 	"net"
 	"net/http"
-	"slices"
 )
 
 //go:embed templates/*.tmpl.html
@@ -17,31 +16,67 @@ type PageData struct {
 	Explanation template.HTML
 }
 
-func main() {
-	cfHosts, _ := net.LookupHost("tunnel")
-	tmpl, err := template.New("html").ParseFS(content, "templates/*.tmpl.html")
-	if err != nil {
-		panic(err)
+var (
+	local, docker *net.IPNet
+)
+
+func init() {
+	local = mustParseCIDR("192.168.0.0/16")
+	docker = mustParseCIDR("172.0.0.0/8")
+}
+
+var (
+	localData = PageData{
+		Title:       "Am I Home? Yes",
+		Message:     "Hello, Local!",
+		Explanation: `<p>Congratulations! It appears your connection is being routed through the local network.</p>`,
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ips := r.Header.Values("X-Forwarded-For")
-		data := PageData{
-			Title:       "Am I Home? Yes",
-			Message:     "Hello, Local!",
-			Explanation: `<p>Congratulations! It appears your connection is being routed through the local network.</p>`,
-		}
-		if HasAny(cfHosts, ips) {
-			data.Title = "Am I Home? No"
-			data.Message = "Hello, World!"
-			data.Explanation = `<p>You are seeing this message because your connection is being routed through the public internet. This may be due to one of the following reasons:</p>
+	dockerData = PageData{
+		Title:   "Am I Home? No",
+		Message: "Hello, World!",
+		Explanation: `<p>You are seeing this message because your connection is being routed through the public internet. This may be due to one of the following reasons:</p>
                 <ol>
                     <li>You are not connected to the network.</li>
                     <li>You might be using a VPN, causing your traffic to be routed outside of the network.</li>
                     <li>Your DNS settings may not be pointing directly to the network gateway.</li>
                     <li>Your DNS cache might still be holding the global address.</li>
                 </ol>
-                <p>To resolve this issue, ensure you are connected to the <code>dbut2</code> network and that your local DNS settings are pointing to the network gateway at <code>192.168.2.1</code>.</p>`
+                <p>To resolve this issue, ensure you are connected to the <code>dbut2</code> network and that your local DNS settings are pointing to the network gateway at <code>192.168.2.1</code>.</p>`,
+	}
+)
+
+func mustParseCIDR(str string) *net.IPNet {
+	_, n, err := net.ParseCIDR(str)
+	if err != nil {
+		panic(err.Error())
+	}
+	return n
+}
+
+func main() {
+	tmpl, err := template.New("html").ParseFS(content, "templates/*.tmpl.html")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ips := MapSlice(r.Header.Values("X-Forwarded-For"), func(ip string) net.IP {
+			return net.ParseIP(ip)
+		})
+
+		var data PageData
+
+		if IsAll(ips, func(ip net.IP) bool {
+			return local.Contains(ip)
+		}) {
+			data = localData
+		}
+
+		if IsAny(ips, func(ip net.IP) bool {
+			return docker.Contains(ip)
+		}) {
+			data = dockerData
 		}
 
 		w.Header().Set("Content-Type", "text/html")
@@ -54,11 +89,28 @@ func main() {
 	}
 }
 
-func HasAny[S ~[]E, E comparable](a, b S) bool {
-	for _, v := range a {
-		if slices.Contains(b, v) {
+func IsAny[S ~[]E, E any](s S, f func(E) bool) bool {
+	for _, e := range s {
+		if f(e) {
 			return true
 		}
 	}
 	return false
+}
+
+func IsAll[S ~[]E, E any](s S, f func(E) bool) bool {
+	for _, e := range s {
+		if !f(e) {
+			return false
+		}
+	}
+	return true
+}
+
+func MapSlice[S ~[]E, E any, U any](s S, f func(E) U) []U {
+	u := make([]U, len(s))
+	for i, e := range s {
+		u[i] = f(e)
+	}
+	return u
 }
