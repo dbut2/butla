264ffebd6e71cd682e8cab8c74134057fd7132d3
Dylan Butler <dylan@but.la>
2024-08-01 11:28:27 +1000

    butla

---
+++ butla/go.mod
@@ -0,0 +1,5 @@
+module github.com/dbutlabs/butla
+
+go 1.22.5
+
+require gopkg.in/yaml.v3 v3.0.1
---
+++ butla/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed config.yaml
+var c []byte
+
+type Config map[string]map[string]string
+
+func main() {
+	con := Config{}
+	sanic(yaml.Unmarshal(c, &con))
+	fmt.Println(con)
+	sanic(http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", getCode(con, r.Host, r.RequestURI))
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})))
+}
+
+func getCode(con Config, host, path string) string {
+	if _, ok := con[host]; !ok {
+		host = "default"
+	}
+	if v, ok := con[host][path]; ok {
+		return v
+	}
+	if v, ok := con[host]["default"]; ok {
+		return v
+	}
+	if v, ok := con["default"][path]; ok {
+		return v
+	}
+	return con["default"]["default"]
+}
+
+func sanic(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
