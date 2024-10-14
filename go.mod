d06d3282ce144440e7aeed0a77a2fe64c92e2e7b
Dylan Butler <dylan@but.la>
2024-10-14 10:59:33 +0000

    stupid simple, stupid fast

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dbut2/butla
+
+go 1.23.2
+
+require gopkg.in/yaml.v3 v3.0.1
--- shortener/pkg/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	Username string `db:"username"`
-	Email    string `db:"email"`
-	Name     string `db:"name"`
-	Password string `db:"password"`
-}
